0b1430e72c34337ea5e99ba88182733755aba288
rjwilson0531 <rjwilson0531@gmail.com>
2020-02-18 10:54:43 -0500

    started lab

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        
+    end
+
+    get '/name' do
+        
+    end
+
+    get '/hometown' do
+        
+    end
+
+    get '/favorite-song' do
+        
+    end
 end
