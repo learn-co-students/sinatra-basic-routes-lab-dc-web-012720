c6132a8a9479e395f19621ee23eedee37d6e971e
rjwilson0531 <rjwilson0531@gmail.com>
2020-02-18 10:56:14 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,18 +2,18 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
     get '/' do
-        
+        "Hello, Ryan!"
     end
 
     get '/name' do
-        
+        "My name is Ryan"
     end
 
     get '/hometown' do
-        
+        "My hometown is Brunswick, MD"
     end
 
     get '/favorite-song' do
-        
+        "My favorite song is Free Bird by Lynard Skynard"
     end
 end
